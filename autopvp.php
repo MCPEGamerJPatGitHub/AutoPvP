5c0417cec76e5682594a8e21f56a099477f03966
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-06-03 18:20:23 +0400

    Update autopvp.php

--- autopvp.php
+++ autopvp.php
@@ -2,19 +2,21 @@
 
 namespace MCPEGamerJP\autopvp
 
-use pocketmine\plugin/pluginbase;
+use pocketmine\plugin\pluginbase;
 
 class autopvp extends pluginbase{
 
       public function onEnable(){
+            $this->getLogger()->info("test");
            }
 
 
       public function onDisable(){
+            $this->getLogger()->info("test");
            }
            
  }
  
  foreach(Server::getInstance()->getOnlinePlayers() as $player){
-    $this->playerList[$player->getName()] = $this->getPlayerData($player);
+    $this->playerList[$player->getName()] = $this->getPlayerData($player); //what am I doing xD
 }
